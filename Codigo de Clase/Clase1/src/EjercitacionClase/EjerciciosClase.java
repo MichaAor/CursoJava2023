9572065a8fb9aae7037f6000a17d0181f83c6067
Bluckiz <misaelflores4190@gmail.com>
2023-12-14 16:26:17 -0300

    Ejercitacion de clase pulida.

--- Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
+++ Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
@@ -2,17 +2,22 @@ package EjercitacionClase;
 
 public class EjerciciosClase {
     public static void main(String[] args) {
-      //Realizar la ejercitacion sobre el este main.
+
 
       /**
+       *                                        //-- EJERCITACION DE CLASE --//
+       * --Realizar la ejercitacion sobre el este main.
        * 1) Declara e inicializa por separado 2 variables, una de tipo primitivo y otra de referencia [String Class]:
        *    a) Imprime ambas usando el metodo sout.
        *    b) Concatena ambas variables y enseña por pantalla el resultado (puede o no usar una variable auxiliar).
        * 2) Corregir la cadena: "                C 3s un l3nguaj3 d3 pr0gramac10n 0r1entad0 a obj3tos, USAD0 PARA HAC3R pag1n4s W3BS.
        * 3) Demuestra tus conocimientos aplicando al menos 4 metodos (no usados anteriormente) y muestralos en la consola.
+       * 4) Realiza las siguientes conversiones:
+       *   a) De cm a mts. => {1cm = 0.01}
+       *   b) De Celsius a Farenheit => {(0°C × 9/5) + 32 = 32F}
+       *   c) De Dolar a Peso => {1 Dolar = 1100 Pesos}
+       * 5) Declarar un numero y mostrar si es par o impar usando el operador 'ternario'
        */
-        String val3 = "Java tiene muchos años. Gracias a los desarrolladores java, sigue creciendo constantmente";
-        System.out.println(val3.indexOf("Java"));
 
     }
 }
