3a36593f9bab99cf0cefb4b1b89ac4b5c2d71a5d
Bluckiz <misaelflores4190@gmail.com>
2023-12-15 13:19:48 -0300

    Clase2, codigo de clase y ejercitacion terminada. Proyecto para casa creado.

--- Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
+++ Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
@@ -10,12 +10,6 @@ public class EjerciciosClase {
        *    b) Concatena ambas variables y enseña por pantalla el resultado (puede o no usar una variable auxiliar).
        * 2) Corregir la cadena: "                C 3s un l3nguaj3 d3 pr0gramac10n 0r1entad0 a obj3tos, USAD0 PARA HAC3R pag1n4s W3BS.
        * 3) Demuestra tus conocimientos aplicando al menos 4 metodos (no usados anteriormente) y muestralos en la consola.
-       * 4) Realiza las siguientes conversiones:
-       *   a) De cm a mts. => {1cm = 0.01}
-       *   b) De Celsius a Farenheit => {(0°C × 9/5) + 32 = 32F}
-       *   c) De Dolar a Peso => {1 Dolar = 1100 Pesos}
-       * 5) Declarar un numero y mostrar si es par o impar usando el operador 'ternario'
        */
-
     }
 }
