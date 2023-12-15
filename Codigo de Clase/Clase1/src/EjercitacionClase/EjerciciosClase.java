7f49368bba5f48a7573bf18f732beb8141617530
Bluckiz <misaelflores4190@gmail.com>
2023-12-14 21:10:27 -0300

    Pequeños cambios

--- Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
+++ Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
@@ -2,8 +2,6 @@ package EjercitacionClase;
 
 public class EjerciciosClase {
     public static void main(String[] args) {
-
-
       /**
        *                                        //-- EJERCITACION DE CLASE --//
        * --Realizar la ejercitacion sobre el este main.
--- Codigo de Clase/Clase1/src/codigoDeClase/ClaseString.java
+++ Codigo de Clase/Clase1/src/codigoDeClase/ClaseString.java
@@ -10,13 +10,14 @@ public class ClaseString {
         String cadena3 = new String("Cadena 2");
 
     //Algunos Metodos Fundamentales
-        String cadena4 = "Fundametales";
-        String cadena5 = "fundametales";
+        String cadena4 = "Fundamentales";
+        String cadena5 = "fundamentales";
         System.out.println("El tamaño de [cadena4] es de " + cadena4.length() + " caracteres");
         System.out.println("El caracter ubicado en la posicion [5] es: " + cadena4.charAt(5));
         System.out.println("Los String 'cadena4' y 'cadena5' son iguales? " + cadena4.equals(cadena5));
         System.out.println("Los String 'cadena4' y 'cadena5' son iguales? " + cadena4.equalsIgnoreCase(cadena5));
+        System.out.println("Los String 'cadena4' y 'cadena5' son iguales? " + (cadena4.compareTo(cadena5)));
         System.out.println("El String 'cadena4' cambia a minusculas: " + cadena4.toLowerCase());
-        System.out.println("La posicion donde comienza la palabra 'metales' es: " + cadena4.indexOf("metales"));
+        System.out.println("La posicion donde comienza la palabra 'mentales' es: " + cadena4.indexOf("mentales"));
     }
 }
