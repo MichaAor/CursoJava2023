4ba44d6feab3bd9d9cb14e7a54b6c0c0fd48e84a
Bluckiz <misaelflores4190@gmail.com>
2023-12-22 14:34:14 -0300

    Clase 6, ejercitacion terminada.

---
+++ Codigo de Clase/Clase6/src/EjercitacionClase/Poliformismo/MainEjercicioPoliformismo.java
@@ -0,0 +1,9 @@
+package EjercitacionClase.Poliformismo;
+
+/**
+ * Utiliza polimorfismo para calcular el salario de manera diferente para cada tipo de empleado.
+ */
+public class MainEjercicioPoliformismo {
+    public static void main(String[] args) {
+    }
+}
