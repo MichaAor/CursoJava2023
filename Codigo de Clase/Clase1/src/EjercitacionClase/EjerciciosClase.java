b7feef2cc3fb1b2563546c811f520721462420bc
Bluckiz <57368872+MichaAor@users.noreply.github.com>
2023-12-26 20:40:53 -0300

    Correccion EjClase 1.
    Se realizo documentacion del uso de algunos metodos, asi como una mejor impresion en consola de todos los ejercicios.

--- Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
+++ Codigo de Clase/Clase1/src/EjercitacionClase/EjerciciosClase.java
@@ -12,13 +12,22 @@ public class EjerciciosClase {
        * 3) Demuestra tus conocimientos aplicando al menos 4 metodos (no usados anteriormente) y muestralos en la consola.
        */
 
+      //Ejercicio 1
+      System.out.println("\033[36m" + "\n//--EJERCICIO 1--//" + "\u001B[0m");
       char variable1 = 'A';
       String variable2 = "A";
+
+      System.out.println("\033[33m" + "--EJERCICIO A--" + "\u001B[0m");
       System.out.println("El valor de variable1 es: " +variable1);
       System.out.println("El valor de variable2 es: " +variable2);
+
+      System.out.println("\033[33m" + "--EJERCICIO B--" + "\u001B[0m");
       System.out.println("variable1 concatenada a variable2 es: "+ variable1 + variable2);
 
+      //Ejercicio 2
+      System.out.println("\033[36m" + "\n//--EJERCICIO 2--//" + "\u001B[0m");
       variable2 = "                C 3s un l3nguaj3 d3 pr0gramac10n 0r1entad0 a obj3tos, USAD0 PARA HAC3R pag1n4s W3BS";
+
       System.out.println("El valor inicial del texto es: "+ variable2);
       variable2 = variable2.trim();
       variable2 = variable2.replace('3','e');
@@ -27,20 +36,38 @@ public class EjerciciosClase {
       variable2 = variable2.toLowerCase();
       variable2 = variable2.replace('4','a');
       System.out.println("El valor final del texto es: "+ variable2);
+      //Correccion: C es un lenguaje POO? Se usa para hacer paginas WEB?
 
+      
+      //Ejercicio 3
+      System.out.println("\033[35m" + "\n//--EJERCICIO 3--//" + "\u001B[0m");
+      System.out.println("Cadena Actual: " + "\033[31m" + variable2 + "\u001B[0m");
 
+      //Convirtiendo toda la cadena a mayuscula con .toUpperCase()
+      System.out.println("\033[33m" + "\nAplico .toUpperCase()" + "\u001B[0m");
       variable2 = variable2.toUpperCase();
       System.out.println("toUpperCase(): "+ variable2);
 
-      System.out.println("length: "+ variable2.length());
+      //Obteniendo extension de cadena con .length()
+      System.out.println("\033[33m" + "\nAplico .toUpperCase()" + "\u001B[0m");
+      System.out.println("Extension de la cadena: "+ variable2.length());
 
+      //Comparando con .equals()
+      System.out.println("\033[33m" + "\nComparando con .equals()" + "\u001B[0m");
       String cadena1 = "Demuestra tus conocimientos aplicando al menos 4 metodos";
       String cadena2 = "Demuestra tus conocimientos aplicando al menos 3 metodos";
+      System.out.println("Cadena 1: " + "\033[31m" + cadena1 + "\u001B[0m");
+      System.out.println("Cadena 2: " + "\033[31m" + cadena2 + "\u001B[0m");
+
       if(cadena1.equals(cadena2)){
-          System.out.println("cadena 1 y cadena 2 son iguales");
+        System.out.println("Resultado de comparacion: " + "\033[32m" + "cadena1 y cadena2 son iguales" + "\u001B[0m");
       } else {
-          System.out.println("cadena 1 y cadena 2 son distintas");
+        System.out.println("Resultado de comparacion: " + "\033[31m" + "cadena1 y cadena2 son diferentes" + "\u001B[0m");
       }
-      System.out.println("Posicion de conocimientos, dentro del texto: " + cadena2 + ". " + "Posicion: " + cadena2.indexOf("conocimientos"));
+
+      //Obteniendo primera posicion de coincidencia con .indexOf(String cadenaABuscar)
+      System.out.println("\033[33m" + "\nComparando con .equals()" + "\u001B[0m");
+      System.out.println("Cadena a filtar: " + "\033[31m" + cadena2 + "\u001B[0m");
+      System.out.println("Posicion de la cadena 'conocimientos' usando .indexOf(): " + cadena2.indexOf("conocimientos"));
     }
 }
