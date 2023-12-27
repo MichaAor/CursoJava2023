814702d23a70b8ed8eae3372d3d860ac8abdffbe
Bluckiz <57368872+MichaAor@users.noreply.github.com>
2023-12-26 21:36:39 -0300

    Correccion EjClase3
    Se reorganizo todo el codigo, se dejaron observaciones de cada ejercicio, ademas de una correccion sobre el 4 ya que no compilaba, revisar observacion.

--- Codigo de Clase/Clase3/src/EjercitacionClase/EjerciciosClase.java
+++ Codigo de Clase/Clase3/src/EjercitacionClase/EjerciciosClase.java
@@ -6,30 +6,12 @@ import static java.lang.Integer.parseInt;
 
 public class EjerciciosClase {
     public static void main(String[] args) {
-      /**
-       *                                        //-- EJERCITACION DE CLASE --//
-       *1) Muestra la tabla de multiplicar de un número introducido por teclado.
-       *2) Realiza un programa que nos diga cuántos dígitos tiene un número introducido
-       *    por teclado limitado a 5 dígitos como máximo y el tipo de dato
-       *    debe ser 'int' o 'long'.
-       *3) Escribe un programa que calcule la media de un conjunto de números positivos
-       *    introducidos por teclado. A priori, el programa no sabe cuántos números se
-       *    introducirán. El usuario indicará que ha terminado de introducir los datos
-       *    cuando meta un número negativo.
-       *4) Escribe un programa que lea una lista de diez números y determine cuántos
-       *    son positivos, y cuántos son negativos.
-       *5) Escribe un programa que pida una base y un exponente (entero positivo) y que
-       *    calcule la potencia..
-       *6) Escribe un programa que diga si un número introducido por teclado es o no
-       *    primo. Un número primo es aquel que sólo es divisible entre él mismo y la unidad.
-       */
+/* Inicio Ejercicio Nro 1 */
+// 1) Muestra la tabla de multiplicar de un número introducido por teclado.
 
-
-        /* Inicio Ejercicio Nro 1 */
-        // 1) Muestra la tabla de multiplicar de un número introducido por teclado.
-/**
-        System.out.print("Ejercicio Nro 1");
-        System.out.print("\033[33m"+"\nIngrese un entero: "+"\u001B[0m");
+/*
+        System.out.println("\033[36m" + "//--EJERCICIO 1--\\" + "\u001B[0m");
+        System.out.print("\033[33m"+"Ingrese un entero: "+"\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
         int numeroEntero = scannerBasic.nextInt();
         for(int i= 1; i<=10; i++){
@@ -37,19 +19,19 @@ public class EjerciciosClase {
         }
         scannerBasic.close();
 */
-        /* Fin Ejercicio Nro 1 */
 
-        /*----------------------------------------------------------------------------------*/
-        /* Inicio Ejercicio Nro 2 */
-        /**
-          2) Realiza un programa que nos diga cuántos dígitos tiene un número introducido
-             por teclado limitado a 5 dígitos como máximo y el tipo de dato
-             debe ser 'int' o 'long'.
-        **/
-/**
+/* Fin Ejercicio Nro 1 */
 
-        System.out.print("Ejercicio Nro 2");
-        System.out.print("\033[33m"+"\nIngrese un entero: "+"\u001B[0m");
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Inicio Ejercicio Nro 2 */
+/* 2) Realiza un programa que nos diga cuántos dígitos tiene un número introducido por teclado limitado a 5 dígitos como máximo
+        y el tipo de dato debe ser 'int' o 'long'.
+*/
+
+/*
+        System.out.println("\033[36m" + "//--EJERCICIO 2--\\" + "\u001B[0m");
+        System.out.print("\033[33m"+"Ingrese un entero: "+"\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
         String cadenaNumerica = scannerBasic.next();
 
@@ -63,21 +45,22 @@ public class EjerciciosClase {
             System.out.println("La longitud del dato ingresado es mayor a 5");
             }
         scannerBasic.close();
- */
-        /* Fin Ejercicio Nro 2 */
-
-        /*----------------------------------------------------------------------------------*/
-        /* Inicio Ejercicio Nro 3 */
-/**
-       *3) Escribe un programa que calcule la media de un conjunto de números positivos
-        *    introducidos por teclado. A priori, el programa no sabe cuántos números se
-        *    introducirán. El usuario indicará que ha terminado de introducir los datos
-        *    cuando meta un número negativo.
 */
-/**
-        System.out.print("Ejercicio Nro 3");
 
-        System.out.print("\033[33m" + "\nIngrese un entero: " + "\u001B[0m");
+/* Fin Ejercicio Nro 2 */
+//Observacion: Perfecto como abordas el ejercicio, pero no me queda claro el uso de las variables 'numeroInt' y 'numeroLong'.
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Inicio Ejercicio Nro 3 */
+/* 3) Escribe un programa que calcule la media de un conjunto de números positivos introducidos por teclado.
+        - A priori, el programa no sabe cuántos números se introducirán.
+        - El usuario indicará que ha terminado de introducir los datos cuando meta un número negativo.
+*/
+
+/*
+        System.out.println("\033[36m" + "//--EJERCICIO 3--\\" + "\u001B[0m");
+        System.out.print("\033[33m" + "Ingrese un entero: " + "\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
         String datoIngresado = scannerBasic.next();
         Integer numeroIngresado = Integer.parseInt(datoIngresado);
@@ -94,30 +77,28 @@ public class EjerciciosClase {
             System.out.println("La media es: " + sumatoria / cantNumeros);
         } else {
             System.out.println("No se ingreso ningun valor ");
-
         }
         scannerBasic.close();
 */
-        /* Fin Ejercicio Nro 3 */
 
-        /*----------------------------------------------------------------------------------*/
-        /* Inicio Ejercicio Nro 4 */
-/**
-               *4) Escribe un programa que lea una lista de diez números y determine cuántos
-                *    son positivos, y cuántos son negativos.
-*/
-/**
-        System.out.print("\033[33m" + "\nIngrese un entero: " + "\u001B[0m");
+/* Fin Ejercicio Nro 3 */
+//Observacion: Muy bien planteado. Hay que revisar la optimizacion de recursos, tanto el Scanner como las variables de referencia.
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Inicio Ejercicio Nro 4 */
+//4) Escribe un programa que lea una lista de diez números y determine cuántos son positivos, y cuántos son negativos.
+
+/*
+        System.out.println("\033[36m" + "//--EJERCICIO 4--\\" + "\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
-        String datoIngresado;
-        Integer numeroIngresado;
-        Integer cantNPositivos = 0;
-        Integer cantNNegativos = 0;
-        Integer cantNumeros = 0;
+        int numeroIngresado;
+        int cantNPositivos = 0;
+        int cantNNegativos = 0;
+        int cantNumeros = 0;
         for(int i=1; i<=10;i++) {
-            System.out.print("\033[33m" + "\nIngrese un entero: " + "\u001B[0m");
-            datoIngresado = scannerBasic.next();
-            numeroIngresado = Integer.parseInt(datoIngresado);
+            System.out.print("\033[33m" + "Ingrese un entero: " + "\u001B[0m");
+            numeroIngresado = scannerBasic.nextInt();
             if(numeroIngresado >= 0) {
                 cantNPositivos++;
             } else {
@@ -126,18 +107,20 @@ public class EjerciciosClase {
         }
         System.out.println("Cantidad de numeros positivos: " + cantNPositivos);
         System.out.println("Cantidad de numeros negativos: " + cantNNegativos);
-        }
         scannerBasic.close();
 */
-    /* Fin Ejercicio Nro 4 */
-    /*----------------------------------------------------------------------------------*/
-    /* Inicio Ejercicio Nro 5 */
-/**
-           *5) Escribe un programa que pida una base y un exponente (entero positivo) y que
-       *    calcule la potencia..
-*/
-/**
-        System.out.print("\033[33m" + "\nIngrese base: " + "\u001B[0m");
+
+/* Fin Ejercicio Nro 4 */
+//Observaciones: A cuidar los recursos, mejorar uso de Scanner, clases primitivas y revisar, 1 variable sin uso y sobra 1 '}'.
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Inicio Ejercicio Nro 5 */
+// 5) Escribe un programa que pida una base y un exponente (entero positivo) y que calcule la potencia.
+
+/*
+        System.out.println("\033[36m" + "//--EJERCICIO 5--\\" + "\u001B[0m");
+        System.out.print("\033[33m" + "Ingrese base: " + "\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
         String datoIngresado = scannerBasic.next();
         Integer base = Integer.parseInt(datoIngresado);
@@ -149,17 +132,20 @@ public class EjerciciosClase {
         double resultadoPotencia = Math.pow(base, exponente);
         System.out.println("Potencia: " + resultadoPotencia);
         scannerBasic.close();
-    }
 */
-    /* Fin Ejercicio Nro 5 */
-    /*----------------------------------------------------------------------------------*/
-    /* Inicio Ejercicio Nro 6 */
-/**
-       *6) Escribe un programa que diga si un número introducido por teclado es o no
-       *    primo. Un número primo es aquel que sólo es divisible entre él mismo y la unidad.
+
+/* Fin Ejercicio Nro 5 */
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Inicio Ejercicio Nro 6 */
+/* 6) Escribe un programa que diga si un número introducido por teclado es o no primo. Un número primo es aquel que sólo es
+        divisible entre él mismo y la unidad.
 */
-/**
-        System.out.print("\033[33m" + "\nIngrese un numero: " + "\u001B[0m");
+
+/*
+        System.out.println("\033[36m" + "//--EJERCICIO 6--\\" + "\u001B[0m");
+        System.out.print("\033[33m" + "Ingrese un numero: " + "\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
         String datoIngresado = scannerBasic.next();
         Integer numeroIngresado = Integer.parseInt(datoIngresado);
@@ -178,9 +164,12 @@ public class EjerciciosClase {
         System.out.println(datoIngresado + " No es un numero primo");
         } else{
             System.out.println(datoIngresado + " Es un numero primo");
-           }
+        }
         scannerBasic.close();
 */
-    /* Fin Ejercicio Nro 6 */
-     }
+
+/* Fin Ejercicio Nro 6 */
+//Observacion: Buen planteo, aunque se puede mejorar y acotar, cuidado los recursos.
+        
     }
+}
