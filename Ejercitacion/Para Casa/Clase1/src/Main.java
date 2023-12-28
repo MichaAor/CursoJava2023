8aeef48c2fcbe679bc56ba5dfa7efc7c8cdef43a
arejmaci <benjaminmacias@gmail.com>
2023-12-28 19:57:30 -0300

    Clase 1 para casa

--- Ejercitacion/Para Casa/Clase1/src/Main.java
+++ Ejercitacion/Para Casa/Clase1/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println();
+        /*
+        1)	Explica con tus propias palabras por qué JAVA es un lenguaje de programación distinto a los demás.
+            Es un lenguaje de programación fuertemente tipado. Es la evolución de lenguajes como pascal y c.
+            Toma de ellos distintas características que lo hace un lenguaje en el cual un programador se siente cómodo programando. Implementa el paradigma de orientación a objetos y posee muuuuchas librerías que permiten cubrir las necesidades de un desarrollador.
+
+        2)	¿Nuestro proyecto JAVA es dependiente de algún sistema operativo en particular? Justifica.
+            NO, es  totalmente independiente del SO donde se ejecuta… Es un lenguaje interpretado por una VM la cual si depende del SO donde se ejecuta. La VM logra que para java sea transparente en que SO se este ejecutando.
+
+        3)	¿Cuál es la principal diferencia entre las variables ‘primitivas’ y ‘de referencia’?
+            Son completamente distintas, las primitivas almacenan datos primitivos, las de referencias almacenan referencias a objetos.
+
+        4)	Enumera 3 proyectos en los que propondría JAVA para llevar a cabo su desarrollo.
+            Dada las características de java hay variedad de proyectos en los cuales aplicar… Sistema de control de stock, Sistema de administracion de clientes, Sistema de alertas tipo dashboard…
+            Donde no utilizaría java… creo que seria mas acertada la pregunta… NO lo aplicaría en situaciones muy muy puntuales donde por ejemplo se requiera un desempeño en performance muy elevado, donde se requiera acceso directo al hardware.
+
+        5)	Arregla este  ‘Haiku (Recuerda poner el título con algún color que te guste) y disfruta el mensaje’:
+            “Dev Haiku.C0dig0 danz4, err0res 3nseñan,desarr0ll0 av4nza, l1neas traz4n.Desafí0s emergen, cima de saber,ideas brot4n, jardín del aprender.En binari0, crece, pr0gram4 el ser.”
+
+       */
+
+
     }
 }
