5c24d61e93a5c2f795c7244a91ecd898b1d25b8b
arejmaci <benjaminmacias@gmail.com>
2023-12-30 20:11:33 -0300

    acceso a una caja fuerte

--- Ejercitacion/Para Casa/Clase3/src/Main.java
+++ Ejercitacion/Para Casa/Clase3/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
          *      que las horas ordinarias (40 primeras horas de trabajo) se pagan a 12 euros la hora.
          *      A partir de la hora 41, se pagan a 16 euros la hora(Se recibe las horas totales por teclado).
          */
-
+/**
         System.out.println("\033[36m" + "//--EJERCICIO 4--\\" + "\u001B[0m");
         System.out.print("\033[33m" + "Ingrese horas totales: " + "\u001B[0m");
         Scanner scannerBasic = new Scanner(System.in);
@@ -21,11 +21,39 @@ public class Main {
             horasOrdinarias = 40;
             horasExtras = totalHoras -40;
             salarioSemanal = (horasOrdinarias * 12) + (horasExtras * 16);
-            System.out.println("El salario semanal es: "+salarioSemanal);
         } else{
             // El calculo NO incluye horas extras.
             salarioSemanal = totalHoras * 12;
-            System.out.println("El salario semanal es: "+salarioSemanal);
+        }
+        System.out.println("El salario semanal es: "+salarioSemanal);
+    }
+*/
+
+    /**
+     * 23)	Realiza el control de acceso a una caja fuerte donde la combinación será ingresada por teclado,
+     *      un número de 4 cifras. Si no acertamos, se nos mostrará el mensaje “Lo siento, esa no es la
+     *      combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto satisfactoriamente”.
+     *      Tendremos cuatro oportunidades para abrir la caja fuerte.
+     */
+
+        System.out.println("\033[36m" + "//--EJERCICIO 5--\\" + "\u001B[0m");
+        Scanner scannerBasic = new Scanner(System.in);
+        String datoIngresado;
+        int totalHoras;
+        String claveSecreta = "1234";
+        int cantIntentosActuales = 0;
+        int cantIntentosMaximo = 4;
+        String claveIngresada = "";
+        do {
+            cantIntentosActuales++;
+            System.out.print("\033[33m" + "Ingrese clave secreta: " + "\u001B[0m");
+            claveIngresada = scannerBasic.next();
+           } while (!claveIngresada.equals(claveSecreta) && cantIntentosActuales < cantIntentosMaximo);
+        if(claveIngresada.equals(claveSecreta)){
+            System.out.println("La caja fuerte se ha abierto satisfactoriamente.");
+        } else {
+            System.out.println("Despues de 4 intentos no pudiste adivinar la clave, seguis siendo pobre.");
         }
     }
+
 }
