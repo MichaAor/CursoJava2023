cf11b9ff97338a592927b70c82cc1099cd8544ad
Melire <melisarenovales@gmail.com>
2024-01-09 20:46:23 -0300

    clase 1 y dos arriba

--- Ejercitacion/Para Casa/Clase1/src/Main.java
+++ Ejercitacion/Para Casa/Clase1/src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
+    /*
+    ! Clase1
+    ! Temas
+    !Introducción a Java.
+    !Estructura y Componentes de Proyecto.
+    !Buenas Prácticas de Codificación en Java.
+    !Variables.
+    !Manejo de Cadenas de Texto [Clase String].
+
+    ?Ejercicios
+    ?1)Explica con tus propias palabras por qué JAVA es un lenguaje de programación distinto a los demás.
+    *Java es un lenguaje de programación orientado a objetos fuertemente tipado, lo que lo diferencia de otros lenguajes es su robustez y su adaptabilidad a diferentes entornos gracias a la java virtual machine.
+
+    ?2)¿Nuestro proyecto JAVA es dependiente de algún sistema operativo en particular? Justifica.
+    *No, ya que la JVM proporciona el entorno adecuado para su utilizacion en diferentes sistemas operativos
+
+
+    ?3)¿Cuál es la principal diferencia entre las variables ‘primitivas’ y ‘de referencia’?
+    *La diferencia entre ambas es que las variables primitivas son datos que se guardan en memoria, ocupan poco espacio, y las variables de referencia son clases que ofrecen diferentes métodos para trabajar con ellas, por ejemplo int (primitiva) -Integer (de referencia) que permite utilizar métodos como suma, compare to, etc.
+
+    ?4)Enumera 3 proyectos en los que propondría JAVA para llevar a cabo su desarrollo.
+    *-app homebanking
+    *-app para control de pagos de gimnasio
+    *-app para puntos de venta de supermercados (omnicanal)
+    */
     public static void main(String[] args) {
-        System.out.println();
+        ej5();
+    }
+    
+//?5)Arregla este  ‘Haiku (Recuerda poner el título con algún color que te guste) y disfruta el mensaje’:
+//?“Dev Haiku.C0dig0 danz4, err0res 3nseñan,desarr0ll0 av4nza, l1neas traz4n.Desafi0s emergen, cima de saber,ideas brot4n, jardin del aprender.En binari0, crece, pr0gram4 el ser.”
+    public static void ej5(){
+        String original = "Dev Haiku.C0dig0 danz4, err0res 3nseñan,desarr0ll0 av4nza, l1neas traz4n.Desafi0s emergen, cima de saber,ideas brot4n, jardin del aprender.En binari0, crece, pr0gram4 el ser.";
+        String corregido;
+
+        //?--Pasos para arreglar la cadena:
+        //**1. Convertir toda la cadena a mayusculas
+        corregido = original.toUpperCase();
+        //*2. Reemplazar 0 por O.
+        corregido = corregido.replace('0','O');
+        //*3. Reemplazar 1 por I.
+        corregido = corregido.replace('1','I');
+        //*4. Reemplazar 4 por A.
+        corregido = corregido.replace('4','A');
+        //*5. Reemplazar 3 por A.
+        corregido = corregido.replace('3','E');
+        System.out.println("La cadena corregida es: " + "\033[32m" + "'" + corregido + "'" + "\u001B[0m");
     }
 }
--- Ejercitacion/Para Casa/Clase2/src/Main.java
+++ Ejercitacion/Para Casa/Clase2/src/Main.java
@@ -1,5 +1,145 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // ej1();
+       //ej2();
+       ej3();
+    }
+      
+      /*
+       * Clase2
+       * Temas
+       * Operadores.
+       * Flujos de Control.
+       * Wrapper.
+       * Ejercicios
+       * Clasifica y menciona al menos 2 ejemplos de Operadores.
+       * operadores aritméticos: +,-,*,/
+       * operadores logicos: &,!,||
+       * 
+       * ¿Cual es la principal diferencia entre las sentencias ‘if’ y switch?
+       * Cuando usamos IF no necesitamos conocer exactamente la cantidad y valor de
+       * los casos a evaluar
+       * Cuando usamos SWITCH es necesario saber exactamente, la cantidad y los
+       * valores de los escenarios a evaluar
+       * 
+       * Explica con tus palabras el funcionamiento de la estructura de control de
+       * iteración ‘foreach’.
+       * La estructura for each te permite recorrer un array tomando cada uno se sus
+       * elementos.
+       * Sirve para cuando no sabes en que posición del array se encuentra el elemento
+       * que estás buscando.
+       * 
+       * 
+       * Declara una variable ‘hora’ de tipo ‘int’ y que muestra los buenos días,
+       * buenas tardes o buenas noches según la hora. Se utilizarán los tramos de 6 a
+       * 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en cuenta las
+       * horas, los minutos no se deben usar.
+       */
+      public static void ej1() {
+          int hora = 7;
+          String saludar;
+          if (hora >= 6 && hora <= 12) {
+              saludar = "Buenos días";
+          } else if (hora >= 13 && hora <= 20) {
+              saludar = "Buenas tardes";
+          } else {
+              saludar = "Buenas noches";
+          }
+          System.out.println(saludar);
+      }
+
+      public static void ej2() {
+          // Realiza un mini cuestionario con 5 preguntas tipo test. Cada pregunta
+          // acertada sumará un punto. El programa mostrará al final la calificación
+          // obtenida.
+          // declaro un scanner para obtener los datos ingresados por el user
+          Scanner sc = new Scanner(System.in);
+
+          // hago la pregunta 1
+          System.out.println(" De que color es el cielo?");
+
+          // leo la respuesta ingresada por el user
+          String respuesta1 = sc.nextLine();
+          // paso la respuesta a mayusculas
+
+          // coloco un contador para sumar el puntaje obtenido por respuesta correcta
+          int puntos = 0;
+
+          if (respuesta1.equalsIgnoreCase("azul")) {
+              System.out.println(" La respuesta es correcta! El cielo es " + respuesta1);
+              puntos++;
+          } else {
+              System.out.println("Respuesta incorrecta! El cielo no es " + respuesta1);
+          }
+          System.out.println("Usted tiene: " + puntos + " puntos");
+
+          // pregunta 2
+          System.out.println(" Cuantas patas tiene un gato?");
+          int respuesta2 = sc.nextInt();
+
+          if (respuesta2 == 4) {
+              System.out.println(" La respuesta es correcta!, Un gato tiene " + respuesta2 + " patas");
+              puntos++;
+          } else {
+              System.out.println("Respuesta incorrecta! Un gato no tiene " + respuesta2 + " patas");
+          }
+          System.out.println("Usted tiene: " + puntos + " puntos");
+
+          sc.nextLine();
+
+          // pregunta 3
+          System.out.println(" Cual es la capital de Italia?");
+          String respuesta3 = sc.nextLine();
+
+          if (respuesta3.equalsIgnoreCase("Roma")) {
+              System.out.println(" la respuesta es correcta! La capital de Italia es:  " + respuesta3);
+              puntos++;
+          } else {
+              System.out.println("Respuesta incorrecta! La capital de Italia no es " + respuesta3);
+          }
+          System.out.println("Usted tiene: " + puntos + " puntos");
+
+          // pregunta 4
+          System.out.println(" La capital de Francia es Roma");
+          boolean respuesta4 = sc.nextBoolean();
+
+          if (respuesta4 == false) {
+              System.out.println(" la respuesta es correcta! La capital de Francia es Paris");
+              puntos++;
+          } else {
+              System.out.println("Respuesta incorrecta! La capital de Francia no es Roma");
+          }
+          System.out.println("Usted tiene: " + puntos + " puntos");
+
+
+          // pregunta 5
+          System.out.println(" El caballo de san martin era blanco");
+          boolean respuesta5 = sc.nextBoolean();
+
+          if (respuesta5 == true) {
+              System.out.println(" la respuesta es correcta! El caballo de San Martin era blanco");
+              puntos++;
+          } else {
+              System.out.println("Respuesta incorrecta! El caballo de San Martin era blanco");
+          }
+          System.out.println("Usted tiene: " + puntos + " puntos");
+
+      }
+
+      public static void ej3() {
+        //Escribe un programa que muestre los n primeros términos de la serie de Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1 y el resto se calcula sumando los dos anteriores, por lo que tendríamos que los términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144... El número n se debe introducir por teclado.
+        System.out.println(" Ingrese un numero");
+        int numero = sc.nextInt();
+        int numero2 = numero++;
+        int fibonacci = 0;
+
+        fo
+
     }
 }
+
+
+
+
+   
