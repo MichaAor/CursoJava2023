60ad31297e6cb0408cc8f6b9221b20fe0aabab67
Bluckiz <misaelflores4190@gmail.com>
2024-01-19 18:45:28 -0300

    Actualizacion Clase 6

--- Codigo de Clase/Clase6/src/codigoDeClase/Encapsulamiento/CuentaBancaria.java
+++ Codigo de Clase/Clase6/src/codigoDeClase/Encapsulamiento/CuentaBancaria.java
@@ -3,19 +3,19 @@ package codigoDeClase.Encapsulamiento;
 public class CuentaBancaria {
     private double saldo;
 
-    // Getter para obtener el saldo
+//! Getter para obtener el saldo
     public double getSaldo() {
         return saldo;
     }
 
-    // Método para depositar dinero
+//! Método para depositar dinero
     public void depositar(double monto) {
         if (monto > 0) {
             saldo += monto;
         }
     }
 
-    // Método para retirar dinero
+//! Método para retirar dinero
     public void retirar(double monto) {
         if (monto > 0 && saldo >= monto) {
             saldo -= monto;
