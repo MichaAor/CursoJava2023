cbd4a4130cea7ccbbfe8acf73d26e9cd9f6451c9
Veronica <dv.veronica2075@gmail.com>
2024-01-23 13:10:48 -0300

    jubilacion

--- Codigo de Clase/Clase4/src/EjercitacionClase/Ejercicio2.java
+++ Codigo de Clase/Clase4/src/EjercitacionClase/Ejercicio2.java
@@ -3,3 +3,26 @@
  *2) Escribe un programa que calcule el factorial de un número ingresado por el usuario,
  *      pero permitiendo calcular el factorial de varios números consecutivos.
  */
+package EjercitacionClase;
+
+//importo libreria para usar scanner, para poder ingresar datos por consola y guardarlos en variables.
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero  de 0 a 16 - para calcular su factorial: ");
+//      guardo en una variable el numero ingresado por consola.
+        int numero = scanner.nextInt();
+//      creo una variable para guardar el factorial del numero ingresado y la inicializo en 1.
+        int factorial = 1;
+//      recorro todos los numeros desde 1 hasta el numero ingresado por consola.
+        for (int i = 1; i <= numero; i++) {
+//      multiplico el factorial por el numero que se esta recorriendo.
+            factorial = factorial * i;
+        }
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+//      Cierro el scanner
+        scanner.close();
+    }
+}
--- Codigo de Clase/Clase4/src/EjercitacionClase/Ejercicio4.java
+++ Codigo de Clase/Clase4/src/EjercitacionClase/Ejercicio4.java
@@ -4,3 +4,35 @@
  *      fecha de jubilación considerando la edad de jubilación estándar
  *      (por ejemplo, 65 años).
  */
+
+package EjercitacionClase;
+
+//importo libreria para usar scanner, para poder ingresar datos por consola y guardarlos en variables.
+import javax.swing.*;
+import java.util.Scanner;
+//importo libreria para usar LocalDate, para poder trabajar con fechas. (java.time.LocalDate)
+import java.time.LocalDate;
+//importo libreria para usar ChronoUnit, para poder calcular la diferencia entre dos fechas. (java.time.temporal.ChronoUnit)
+import java.time.temporal.ChronoUnit;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Calculemos que dia te jubilas!");
+//        abro el scanner y solicito al usuario que ingrese su fecha de nacimiento.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su fecha de nacimiento (yyyy-mm-dd): ");
+//        guardo en una variable la fecha ingresada por consola.
+        String fechaNacimiento = scanner.nextLine();
+//        convierto la fecha ingresada a LocalDate y la guardo en una variable parseada porque el metodo between de
+//        ChronoUnit recibe LocalDate y no String.
+        LocalDate fechaNacimientoLocalDate = LocalDate.parse(fechaNacimiento);
+
+        // Calculo la fecha que el usuario se jubila, sumando 65 años a su fecha de nacimiento.
+        LocalDate fechaJubilacion = fechaNacimientoLocalDate.plusYears(65);
+
+//        muestro por consola la fecha de jubilacion del usuario.
+        System.out.println("Su fecha de jubilacion es: " + fechaJubilacion);
+//        Cierro el scanner
+        scanner.close();
+    }
+}
